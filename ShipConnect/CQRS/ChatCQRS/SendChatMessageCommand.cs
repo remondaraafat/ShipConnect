96ff843373dd0b20c10ace1c7f3a8fa2e02a73dc
remondaraafat <remondarafat@gmail.com>
2025-07-14 21:28:57 +0300

    edit send message controller

--- ShipConnect/CQRS/ChatCQRS/SendChatMessageCommand.cs
+++ ShipConnect/CQRS/ChatCQRS/SendChatMessageCommand.cs
@@ -6,16 +6,17 @@ using ShipConnect.Models;
 
 namespace ShipConnect.CQRS.ChatCQRS
 {
-    public class SendChatMessageCommand : IRequest<SendChatMessageDTO>
+    public class SendChatMessageCommand : IRequest<GeneralResponse<SendChatMessageDTO>>
     {
         public SendChatMessageRequestDTO DTO { get; set; } 
     }
-    public class SendChatMessageCommandHandler : IRequestHandler<SendChatMessageCommand, SendChatMessageDTO>
+    public class SendChatMessageCommandHandler : IRequestHandler<SendChatMessageCommand, GeneralResponse<SendChatMessageDTO>>
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IHubContext<ChatHub> _hubContext;
-        public async Task<SendChatMessageDTO> Handle(SendChatMessageCommand request, CancellationToken cancellationToken)
+        public async Task<GeneralResponse<SendChatMessageDTO>> Handle(SendChatMessageCommand request, CancellationToken cancellationToken)
         {
+            try { 
             ChatMessage msg = new ChatMessage
             {
                 Content = request.DTO.Content,
@@ -39,7 +40,13 @@ namespace ShipConnect.CQRS.ChatCQRS
             await _hubContext.Clients
                 .User(msg.ReceiverId)
                 .SendAsync("ReceiveMessage", DTO, cancellationToken);
-            return DTO;
+
+            return GeneralResponse<SendChatMessageDTO>.SuccessResponse(data: DTO);
         }
+        catch (Exception ex)
+        {
+            return GeneralResponse<SendChatMessageDTO>.FailResponse(message: ex.Message);
+        }
+}
     }
 }
---
+++ ShipConnect/Controllers/ChatController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShipConnect.CQRS.ChatCQRS;
+using ShipConnect.DTOs.ChatDTOs;
+
+namespace ShipConnect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ChatController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("send")]
+        public async Task<GeneralResponse<SendChatMessageDTO>> Send([FromBody] SendChatMessageRequestDTO dto)
+        {
+            var command = new SendChatMessageCommand { DTO = dto };
+            return await _mediator.Send(command);
+            
+        }
+    }
+}
