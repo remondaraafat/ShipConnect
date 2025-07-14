a4e47a4479fc90e1f9ca883885f125d50424c270
remondaraafat <remondarafat@gmail.com>
2025-07-14 21:09:29 +0300

    send message signalr

--- ShipConnect/CQRS/ChatCQRS/SendChatMessageCommand.cs
+++ ShipConnect/CQRS/ChatCQRS/SendChatMessageCommand.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.SignalR;
 using ShipConnect.DTOs.ChatDTOs;
 using ShipConnect.Hubs;
+using ShipConnect.Models;
 
 namespace ShipConnect.CQRS.ChatCQRS
 {
@@ -9,13 +10,36 @@ namespace ShipConnect.CQRS.ChatCQRS
     {
         public SendChatMessageRequestDTO DTO { get; set; } 
     }
-    //public class SendChatMessageCommandHandler : IRequestHandler<SendChatMessageCommand, SendChatMessageDTO>
-    //{
-    //    //private readonly IUnitOfWork _unitOfWork;
-    //    //private readonly IHubContext<ChatHub> _hubContext;
-    //    //public Task<SendChatMessageDTO> Handle(SendChatMessageCommand request, CancellationToken cancellationToken)
-    //    //{
-            
-    //    //}
-    //}
+    public class SendChatMessageCommandHandler : IRequestHandler<SendChatMessageCommand, SendChatMessageDTO>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IHubContext<ChatHub> _hubContext;
+        public async Task<SendChatMessageDTO> Handle(SendChatMessageCommand request, CancellationToken cancellationToken)
+        {
+            ChatMessage msg = new ChatMessage
+            {
+                Content = request.DTO.Content,
+                SenderId = request.DTO.SenderId,
+                CreatedAt = DateTime.UtcNow,
+                ReceiverId = request.DTO.ReceiverId,
+                IsRead = false,
+                ShipmentId=request.DTO.ShipmentId
+            };
+            _unitOfWork.ChatMessageRepository.AddAsync(msg);
+            await _unitOfWork.SaveAsync();
+
+            SendChatMessageDTO DTO = new SendChatMessageDTO
+            {
+                ShipmentId = msg.Id,
+                Content = msg.Content,
+                SenderId = msg.SenderId,
+                ReceiverId = msg.ReceiverId,
+                IsRead = msg.IsRead
+            };
+            await _hubContext.Clients
+                .User(msg.ReceiverId)
+                .SendAsync("ReceiveMessage", DTO, cancellationToken);
+            return DTO;
+        }
+    }
 }
